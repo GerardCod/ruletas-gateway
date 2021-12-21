c2fcccff414b5e272d97dcff81bfe819092ac03d
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-12-21 09:51:28 -0600

    add: First commit

---
+++ src/main/java/dev/gerardo/microservices/ruletas/gateway/RoutingConfiguration.java
@@ -0,0 +1,19 @@
+package dev.gerardo.microservices.ruletas.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutingConfiguration {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		System.out.println("Creando el router");
+		return builder.routes()
+				.route("ruletas", p -> p.path("/app-service/**").uri("lb://RULETAS-SERVICE"))
+				.build();
+	}
+	
+}
---
+++ src/test/java/dev/gerardo/microservices/ruletas/gateway/RuletasGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gerardo.microservices.ruletas.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuletasGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
